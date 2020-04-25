67ac169de9a6143097ee319242047cfe405ae297
GitHub Action <action@github.com>
2020-04-25 07:37:03 +0000

    CI

--- docs/js/gallery.js
+++ docs/js/gallery.js
@@ -1,15 +1,1 @@
-<<<<<<< HEAD
-document.addEventListener('DOMContentLoaded', function () {
-    if (typeof ($.fn.lightGallery) === 'function') {
-        $('.article').lightGallery({ selector: '.gallery-item' });
-    }
-    if (typeof ($.fn.justifiedGallery) === 'function') {
-        if ($('.justified-gallery > p > .gallery-item').length) {
-            $('.justified-gallery > p > .gallery-item').unwrap();
-        }
-        $('.justified-gallery').justifiedGallery();
-    }
-});
-=======
 "use strict";document.addEventListener("DOMContentLoaded",function(){"function"==typeof $.fn.lightGallery&&$(".article").lightGallery({selector:".gallery-item"}),"function"==typeof $.fn.justifiedGallery&&($(".justified-gallery > p > .gallery-item").length&&$(".justified-gallery > p > .gallery-item").unwrap(),$(".justified-gallery").justifiedGallery())});
->>>>>>> f4bcb6d130eff003a8064c877e0e38a67514e600
